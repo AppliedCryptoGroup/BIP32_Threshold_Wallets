f8d8e60a62cc1c6aaf8217d653955fff81b5d8c7
Philipp-Florens Lehwalder <philipp.lehwalder@gmail.com>
2024-03-26 14:16:40 +0100

    wip

--- node/device.go
+++ node/device.go
@@ -1,6 +1,11 @@
 package node
 
-import "github.com/coinbase/kryptology/pkg/tecdsa/gg20/dealer"
+import (
+	"github.com/coinbase/kryptology/pkg/core/curves"
+	v1 "github.com/coinbase/kryptology/pkg/sharing/v1"
+	"github.com/coinbase/kryptology/pkg/tecdsa/gg20/dealer"
+	"golang.org/x/crypto/sha3"
+)
 
 type SecretKeyShare *dealer.Share
 
@@ -9,6 +14,31 @@ type Device struct {
 	state State
 
 	deviceIdx      int // Index of the device with respect to the secret sharing.
-	secretKeyShare SecretKeyShare
+	t              uint32
+	n              uint32
+	secretKeyShare v1.ShamirShare
 	publicKey      *PublicKey
+}
+
+var hash = sha3.New256()
+var curve = curves.K256()
+
+func (d *Device) RandSk(rho curves.Element) *v1.ShamirShare {
+	// For k in [t]: a <- H(rho || k)
+	// Let f(x) = rho + a_1*x + ... + a_{t-1}*x^{t-1} + a_t*x^t
+	// rho_i = f(i) mod q
+	// sk_i' = sk_i + rho_i
+	// return sk_i'
+
+	aks := make([]curves.Scalar, d.t)
+	var err error
+	for i := uint32(0); i < d.t; i++ {
+		akiBytes := hash.Sum(append(rho.Bytes(), []byte{byte(i)}...))
+		aks[i], err = curve.Scalar.SetBytes(akiBytes)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return nil
 }
