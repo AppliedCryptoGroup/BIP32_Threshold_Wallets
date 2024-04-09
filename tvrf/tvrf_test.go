2e3aff774ee6222d0d2c435b530866f0dd4eae0b
Philipp-Florens Lehwalder <philipp.lehwalder@gmail.com>
2024-04-09 13:32:47 +0200

    test(TVRF): Test combination

--- tvrf/tvrf_test.go
+++ tvrf/tvrf_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/coinbase/kryptology/pkg/core/curves"
 	"github.com/coinbase/kryptology/pkg/tecdsa/gg20/dealer"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"golang.org/x/crypto/sha3"
 
 	"bip32_threshold_wallet/tvrf"
@@ -22,14 +23,53 @@ func TestTVRF(t *testing.T) {
 	p256ec, _ := p256.ToEllipticCurve()
 	secret, _ := dealer.NewSecret(p256ec)
 
-	pk, sharesMap, _ := dealer.NewDealerShares(p256ec, threshold, numParties, secret)
-	_ = pk
+	_, sharesMap, _ := dealer.NewDealerShares(p256ec, threshold, numParties, secret)
 
 	ddhTvrf := tvrf.NewDDHTVRF(threshold, numParties, p256, sha256)
 
 	message := []byte("Hello, World!")
-	err, ski, pki := tvrf.ShamirShareToKeyPair(p256, sharesMap[1].ShamirShare, sharesMap[1].Point)
-	assert.NoError(t, err)
 
-	ddhTvrf.PEval(message, ski, *pki)
+	var secretKeys []tvrf.SecretKeyShare
+	var publicKeys []tvrf.PublicKeyShare
+	for i := uint32(1); i <= numParties; i++ {
+		err, ski, pki := tvrf.ShamirShareToKeyPair(p256, sharesMap[i].ShamirShare, sharesMap[i].Point)
+		require.NoError(t, err)
+		secretKeys = append(secretKeys, ski)
+		publicKeys = append(publicKeys, *pki)
+	}
+
+	t.Run("Verify partial evaluation", func(t *testing.T) {
+		peval, err := ddhTvrf.PEval(message, secretKeys[0], publicKeys[0])
+		assert.NoError(t, err)
+
+		// Verify the evaluation
+		valid := ddhTvrf.VerifyPartialEval(peval)
+		assert.Truef(t, valid, "evaluation verification failed")
+
+		fakeEval := &tvrf.PartialEvaluation{
+			PubKeyShare: peval.PubKeyShare,
+			Eval:        peval.Eval.Mul(p256.Scalar.New(3)),
+			Proof:       peval.Proof,
+		}
+		notValid := ddhTvrf.VerifyPartialEval(fakeEval)
+		assert.Falsef(t, notValid, "evaluation should not be valid")
+	})
+
+	t.Run("Combine partial evaluations", func(t *testing.T) {
+		pevals := make([]*tvrf.PartialEvaluation, 0)
+		for i := uint32(0); i < threshold; i++ {
+			peval, err := ddhTvrf.PEval(message, secretKeys[i], publicKeys[i])
+			require.NoError(t, err)
+			pevals = append(pevals, peval)
+		}
+
+		eval, err := ddhTvrf.Combine(pevals)
+		require.NoError(t, err)
+
+		// Verify the evaluation
+		valid, err := ddhTvrf.Verify(*eval)
+		assert.NoError(t, err)
+		assert.Truef(t, valid, "evaluation verification failed")
+	})
+
 }
